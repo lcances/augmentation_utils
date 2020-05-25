4c9289d0fec6070803779aae78a583548d686d63
leocances <leocances@gmail.com>
2020-05-25 21:34:02 +0200

    refactoring

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='augmentation_utils',
+    version='1.0.0',
+    url='https://github.com/leocances/augmentation_utils.git',
+    author='Leo Cances',
+    author_email='leocances@gmail.com',
+    description='Ensemble of augmentation based on signal, spectrogram and image',
+    packages=find_packages(),
+)
